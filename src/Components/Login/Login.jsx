d57741fcd1bc0921b7e34ac2ce9a9ce63ad1b45c
Lucas Barros <lucasbarros.developer@gmail.com>
2024-06-18 20:38:27 -0300

    Implementação de pagina de login

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,58 @@
+import { FaUser, FaLock } from 'react-icons/fa';
+import { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("Enviando os dados: " + username + " " + password);
+    };
+
+
+    return (
+        <div className='container'>
+            <form onSubmit={handleSubmit}>
+                <h1>Efetue o Login</h1>
+                <div className='input-field'>
+                    <input
+                        type='email'
+                        placeholder='E-mail'
+                        value={username}
+                        onChange={(e) => handleUsernameChange(e)}
+                    />
+                    <FaUser className='icon' />
+                </div>
+                <div className='input-field'>
+                    <input
+                        type='password'
+                        placeholder='Senha'
+                        value={password}
+                        onChange={(e) => handlePasswordChange(e)}
+                    />
+                    <FaLock className='icon' />
+                </div>
+
+                <div className='recall-forget'>
+                    <label>
+                        <input type='checkbox' />
+                        Lembre de mim
+                    </label>
+                    <a href='#'>Esqueci minha Senha</a>
+                </div>
+
+                <button>Entrar</button>
+
+                <div className='signup-link'>
+                    <p>
+                        Não tem uma conta? <a href='#'>Cadastrar-se</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
