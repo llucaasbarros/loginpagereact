0266d0443512c167094e2044c20eb3679b6f9c91
Lucas Barros <lucasbarros.developer@gmail.com>
2024-06-20 16:50:38 -0300

    Adicionando Basico Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import { useRef } from 'react'
+
+const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classicList.toggle("responsive_nav")
+  }
+
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav ref={navRef}>
+        <a href='#'>Home</a>
+        <a href='#'>Sobre</a>
+        <a href='#'>Blog</a>
+        <a href='#'>About me</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes/>
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars/>
+      </button>
+    </header>
+  )
+}
+
+export default Navbar
