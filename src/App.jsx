241002444da63af9ecbe54ad707ecd4cab55199b
Lucas Barros <lucasbarros.developer@gmail.com>
2024-06-22 07:10:23 -0300

    Adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
+import './App.css';
 import Login from "./Components/Login/Login";
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 import React from 'react'; // Importa React para poder usar React.Fragment
-
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className='App'><Login /></div>
+      <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
