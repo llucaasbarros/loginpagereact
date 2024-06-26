ebc4acd6f2dbc689c6b8a6838a8cecd1adf9d934
Lucas Barros <lucasbarros.developer@gmail.com>
2024-06-26 15:07:46 -0300

    Add mudanças

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,26 +2,29 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import praia1 from 'assets/praia1.jpg';
+import praia2 from 'assets/praia2.jpg';
+import praia3 from 'assets/praia3.jpg';
 
 function Accordeon() {
   return (
     <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <ExampleCarouselImage src={praia1} alt="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <ExampleCarouselImage src={praia2} alt="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
+        <ExampleCarouselImage src={praia3} alt="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
